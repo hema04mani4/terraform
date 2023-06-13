373e81b6717d5ee987d2602a0136b29158ad44a0
hema04mani4 <40386345+hema04mani4@users.noreply.github.com>
2023-06-13 06:40:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
