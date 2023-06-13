d01172397965981c00b7de04154669fdd6992544
hema04mani4 <40386345+hema04mani4@users.noreply.github.com>
2023-06-13 03:41:35 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,17 @@
-
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.61.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rghm"
+    storage_account_name = "hemastorage2104"
+    container_name       = "hmcont"
+    key                  = "terraform.tfstate"
+}
+}
+provider "azurerm" {
+  # Configuration options
+}
