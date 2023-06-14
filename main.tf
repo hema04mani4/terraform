8cf16cec878c0ef3636a19fdb279bbdcf90e8933
hema04mani4 <40386345+hema04mani4@users.noreply.github.com>
2023-06-14 02:03:22 -0400

    test

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
 
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+  tags = {
+        terraform_managed = true,
+        terraform_created true
+  }
+}
