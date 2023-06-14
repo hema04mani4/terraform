9df09d35b2dc47379ebfb0133c9b63c0a0a49a3a
hema04mani4 <40386345+hema04mani4@users.noreply.github.com>
2023-06-14 03:10:27 -0400

    Update first.tf

--- first.tf
+++ first.tf
@@ -1,13 +1,13 @@
 
 
-resource "azurerm_resource_group" "example" {
-  name     = var.rgname1
-  location = var.rglocation1
-}
-resource "azurerm_resource_group" "example2" {
-  name     = "example-resources2"
-  location = "West Europe"
-}
-output "myrg"{
-  value = azurerm_resource_group.example
-}
+#resource "azurerm_resource_group" "example" {
+ # name     = var.rgname1
+  #location = var.rglocation1
+#}
+#resource "azurerm_resource_group" "example2" {
+ # name     = "example-resources2"
+  #location = "West Europe"
+#}
+#output "myrg"{
+ # value = azurerm_resource_group.example
+#}
